41c8d85591cd600413798b13e4b1942535ef75da
Ivan Enzhaev <8observer8@gmail.com>
2019-09-03 19:27:28 +0400

    Get a code from the Create Window lesson

---
+++ 001. Getting Started/001. Creating a Window/Game.cs
@@ -0,0 +1,28 @@
+﻿
+using OpenTK;
+using OpenTK.Graphics;
+using OpenTK.Input;
+
+namespace _001.Creating_a_Window
+{
+    class Game : GameWindow
+    {
+        public Game(int width, int height, string title) : base(width, height, GraphicsMode.Default, title)
+        {
+
+        }
+
+        protected override void OnUpdateFrame(FrameEventArgs e)
+        {
+            // Get the state of the keyboard this frame
+            KeyboardState input = Keyboard.GetState();
+
+            if (input.IsKeyDown(Key.Escape))
+            {
+                Exit();
+            }
+
+            base.OnUpdateFrame(e);
+        }
+    }
+}
